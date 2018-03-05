af87eab29b4cf731af5f49647dc3fe49cf9439b0
josy issac <vishal.patil@wwindia.com>
2018-03-05 12:02:15 +0530

    modified the activity and added new file Test add

--- app/src/main/java/com/example/webwerks/newapplication/MainActivity.java
+++ app/src/main/java/com/example/webwerks/newapplication/MainActivity.java
@@ -11,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this,"Hi Git",Toast.LENGTH_LONG).show();
+
+
+
     }
+
+
 }
---
+++ app/src/main/java/com/example/webwerks/newapplication/Testadd.java
@@ -0,0 +1,9 @@
+package com.example.webwerks.newapplication;
+
+/**
+ * Created by webwerks on 5/3/18.
+ */
+
+public class Testadd {
+
+}
